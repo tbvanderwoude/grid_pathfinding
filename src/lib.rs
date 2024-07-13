b74792f5f3bc04b2b8b3e0c4935a0152ef9dee8b
Thom van der Woude <tbvanderwoude@gmail.com>
2024-07-13 11:22:01 +0200

    Added missing diagonals to generate_components

--- src/lib.rs
+++ src/lib.rs
@@ -420,12 +420,16 @@ impl PathingGrid {
                     let neighbours = if self.allow_diagonal_move {
                         vec![
                             Point::new(point.x, point.y + 1),
+                            Point::new(point.x, point.y - 1),
+                            Point::new(point.x + 1, point.y),
+                            Point::new(point.x + 1, point.y - 1),
                             Point::new(point.x + 1, point.y),
                             Point::new(point.x + 1, point.y + 1),
                         ]
                     } else {
                         vec![
                             Point::new(point.x, point.y + 1),
+                            Point::new(point.x, point.y - 1),
                             Point::new(point.x + 1, point.y),
                         ]
                     }
