b49e111b7fc01942334ee5adb24b0a6faed373ee
Thom van der Woude <tbvanderwoude@protonmail.com>
2024-07-26 12:46:13 +0200

    Extended benchmark to all algorithm variants

--- benches/comparison_bench.rs
+++ benches/comparison_bench.rs
@@ -4,11 +4,13 @@ use grid_util::grid::Grid;
 use grid_util::point::Point;
 use rand::prelude::*;
 
-fn random_grid(n: usize, rng: &mut StdRng) -> PathingGrid {
+fn random_grid(n: usize, rng: &mut StdRng, allow_diag: bool, pruning: bool, fill_rate: f64) -> PathingGrid {
     let mut pathing_grid: PathingGrid = PathingGrid::new(n, n, false);
+    pathing_grid.allow_diagonal_move = allow_diag;
+    pathing_grid.improved_pruning = pruning;
     for x in 0..pathing_grid.width() {
         for y in 0..pathing_grid.height() {
-            pathing_grid.set(x, y, rng.gen_bool(0.4))
+            pathing_grid.set(x, y, rng.gen_bool(fill_rate))
         }
     }
     pathing_grid.generate_components();
@@ -26,39 +28,55 @@ fn test(pathing_grid: &PathingGrid, start: Point, end: Point) -> Option<Vec<Poin
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    const N: usize = 64;
-    const N_GRIDS: usize = 1000;
-    const N_PAIRS: usize = 1000;
-    let mut rng = StdRng::seed_from_u64(0);
-    let mut random_grids: Vec<PathingGrid> = Vec::new();
-    for _ in 0..N_GRIDS {
-        random_grids.push(random_grid(N, &mut rng))
+    for (allow_diag, pruning) in [(false, false), (true, false), (true, true)]{
+        const N: usize = 64;
+        const N_GRIDS: usize = 1000;
+        const N_PAIRS: usize = 1000;
+        let mut rng = StdRng::seed_from_u64(0);
+        let mut random_grids: Vec<PathingGrid> = Vec::new();
+        for _ in 0..N_GRIDS {
+            random_grids.push(random_grid(N, &mut rng, allow_diag, pruning, 0.4))
+        }
+    
+        let start = Point::new(0, 0);
+        let end = Point::new(N as i32 - 1, N as i32 - 1);
+        let diag_str = if allow_diag{
+            "8-grid"
+        }
+        else{
+            "4-grid"
+        };
+        let improved_str = if pruning{
+            "(improved pruning)"
+        }
+        else{
+            ""
+        };
+        c.bench_function(format!("1000 random 64x64 {diag_str}s {improved_str}").as_str(), |b| {
+            b.iter(|| {
+                for grid in &random_grids {
+                    test(grid, start, end);
+                }
+            })
+        });
+        let grid = &random_grids[0];
+        let mut random_pairs: Vec<(Point, Point)> = Vec::new();
+        for _ in 0..N_PAIRS {
+            random_pairs.push((
+                random_grid_point(&grid, &mut rng),
+                random_grid_point(&grid, &mut rng),
+            ))
+        }
+        c.bench_function(format!("1000 random start goal pairs on a 64x64 {diag_str} {improved_str}").as_str(), |b| {
+            b.iter(|| {
+                for (start, end) in &random_pairs {
+                    test(&grid, start.clone(), end.clone());
+                }
+            })
+        });
     }
 
-    let start = Point::new(0, 0);
-    let end = Point::new(N as i32 - 1, N as i32 - 1);
-    c.bench_function("1000 random 64x64 grids", |b| {
-        b.iter(|| {
-            for grid in &random_grids {
-                test(grid, start, end);
-            }
-        })
-    });
-    let grid = &random_grids[0];
-    let mut random_pairs: Vec<(Point, Point)> = Vec::new();
-    for _ in 0..N_PAIRS {
-        random_pairs.push((
-            random_grid_point(&grid, &mut rng),
-            random_grid_point(&grid, &mut rng),
-        ))
-    }
-    c.bench_function("1000 random start goal pairs on a 64x64 grid", |b| {
-        b.iter(|| {
-            for (start, end) in &random_pairs {
-                test(&grid, start.clone(), end.clone());
-            }
-        })
-    });
+
 }
 
 criterion_group!(benches, criterion_benchmark);
