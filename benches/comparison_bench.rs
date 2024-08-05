6622dc16de3eb0c455cf37c161ea230ac942d869
Thom van der Woude <tbvanderwoude@protonmail.com>
2024-08-05 12:42:10 +0200

    Changed dao_bench to run maps dao/arena, dao/den312d, dao/arena2

--- benches/comparison_bench.rs
+++ benches/comparison_bench.rs
@@ -1,5 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use grid_pathfinding::PathingGrid;
+use grid_pathfinding_benchmark::*;
 use grid_util::grid::Grid;
 use grid_util::point::Point;
 use rand::prelude::*;
@@ -31,6 +32,31 @@ fn random_grid_point(grid: &PathingGrid, rng: &mut StdRng) -> Point {
 
 fn test(pathing_grid: &PathingGrid, start: Point, end: Point) -> Option<Vec<Point>> {
     black_box(pathing_grid.get_path_single_goal(start, end, false))
+}
+
+fn dao_bench(c: &mut Criterion) {
+    let allow_diag = true;
+    let pruning = false;
+    for name in ["dao/arena", "dao/den312d", "dao/arena2"] {
+        let (bool_grid, scenarios) = get_benchmark(name.to_owned());
+        let mut pathing_grid: PathingGrid =
+            PathingGrid::new(bool_grid.width, bool_grid.height, true);
+        pathing_grid.grid = bool_grid.clone();
+        pathing_grid.allow_diagonal_move = allow_diag;
+        pathing_grid.improved_pruning = pruning;
+        pathing_grid.update_all_neighbours();
+        pathing_grid.generate_components();
+        let diag_str = if allow_diag { "8-grid" } else { "4-grid" };
+        let improved_str = if pruning { " (improved pruning)" } else { "" };
+
+        c.bench_function(format!("{name}, {diag_str}{improved_str}").as_str(), |b| {
+            b.iter(|| {
+                for (start, end) in &scenarios {
+                    test(&pathing_grid, *start, *end);
+                }
+            })
+        });
+    }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -79,5 +105,5 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, dao_bench);
 criterion_main!(benches);
