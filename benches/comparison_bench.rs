d370bf69932b8a47c461f830df43f2031167e78b
Thom van der Woude <tbvanderwoude@protonmail.com>
2025-11-23 20:39:57 +0100

    Introduced generic dao_bench_solver, reusing much of the benchmarking logic for the four implementors of GridSolver

--- benches/comparison_bench.rs
+++ benches/comparison_bench.rs
@@ -46,40 +46,35 @@ fn dao_bench<const ALLOW_DIAGONAL: bool>(c: &mut Criterion) {
     }
 }
 
-fn dao_bench_jps<const ALLOW_DIAGONAL: bool>(c: &mut Criterion) {
-    let arr: SmallVec<[bool; 2]> = if ALLOW_DIAGONAL {
-        smallvec![false, true]
-    } else {
-        smallvec![false]
-    };
+fn dao_bench_solver<const ALLOW_DIAGONAL: bool, S, FS>(
+    c: &mut Criterion,
+    solver_name: &str,
+    create_solver: FS,
+) where
+    S: GridSolver,
+    FS: Fn(&mut PathingGrid<ALLOW_DIAGONAL>) -> S,
+{
     let bench_set = if ALLOW_DIAGONAL {
         ["dao/arena", "dao/den312d", "dao/arena2"]
     } else {
         ["dao/arena", "dao/den009d", "dao/den312d"]
     };
-    for pruning in arr {
-        for name in bench_set {
-            let (bool_grid, scenarios) = get_benchmark(name.to_owned());
-            let mut pathing_grid: PathingGrid<ALLOW_DIAGONAL> =
-                PathingGrid::new(bool_grid.width, bool_grid.height, true);
-            pathing_grid.grid = bool_grid.clone();
-            pathing_grid.generate_components();
-            let mut solver = JPSSolver::new(&pathing_grid, pruning);
-            solver.initialize(&pathing_grid);
-            let diag_str = if ALLOW_DIAGONAL { "8-grid" } else { "4-grid" };
-            let improved_str = if pruning { " (improved pruning)" } else { "" };
+    for name in bench_set {
+        let (bool_grid, scenarios) = get_benchmark(name.to_owned());
+        let mut pathing_grid: PathingGrid<ALLOW_DIAGONAL> =
+            PathingGrid::new(bool_grid.width, bool_grid.height, true);
+        pathing_grid.grid = bool_grid.clone();
+        pathing_grid.generate_components();
+        let solver = create_solver(&mut pathing_grid);
+        let diag_str = if ALLOW_DIAGONAL { "8-grid" } else { "4-grid" };
 
-            c.bench_function(
-                format!("{name}, JPS {diag_str}{improved_str}").as_str(),
-                |b| {
-                    b.iter(|| {
-                        for (start, end, _) in &scenarios {
-                            black_box(solver.get_path_single_goal(&mut pathing_grid, *start, *end));
-                        }
-                    })
-                },
-            );
-        }
+        c.bench_function(format!("{name}, {solver_name} {diag_str}").as_str(), |b| {
+            b.iter(|| {
+                for (start, end, _) in &scenarios {
+                    black_box(solver.get_path_single_goal(&mut pathing_grid, *start, *end));
+                }
+            })
+        });
     }
 }
 pub fn generate_landmarks_mc<const ALLOW_DIAGONAL: bool>(
@@ -99,81 +94,58 @@ pub fn generate_landmarks_mc<const ALLOW_DIAGONAL: bool>(
     }
     landmarks
 }
-fn dao_bench_alt<const ALLOW_DIAGONAL: bool>(c: &mut Criterion) {
-    let bench_set = if ALLOW_DIAGONAL {
-        ["dao/arena", "dao/den312d", "dao/arena2"]
-    } else {
-        ["dao/arena", "dao/den009d", "dao/den312d"]
-    };
-    for name in bench_set {
-        let (bool_grid, scenarios) = get_benchmark(name.to_owned());
-        let mut pathing_grid: PathingGrid<ALLOW_DIAGONAL> =
-            PathingGrid::new(bool_grid.width, bool_grid.height, true);
-        pathing_grid.grid = bool_grid.clone();
-        pathing_grid.generate_components();
-        let landmarks = generate_landmarks_mc(&pathing_grid, 128);
-        let solver = ALTSolver::new(landmarks, &mut pathing_grid);
-        let diag_str = if ALLOW_DIAGONAL { "8-grid" } else { "4-grid" };
 
-        c.bench_function(format!("{name}, ALT (MC) {diag_str}").as_str(), |b| {
-            b.iter(|| {
-                for (start, end, _) in &scenarios {
-                    black_box(solver.get_path_single_goal(&mut pathing_grid, *start, *end));
-                }
-            })
-        });
+fn dao_bench_jps<const ALLOW_DIAGONAL: bool>(c: &mut Criterion) {
+    let arr: SmallVec<[bool; 2]> = if ALLOW_DIAGONAL {
+        smallvec![false, true]
+    } else {
+        smallvec![false]
+    };
+    for pruning in arr {
+        let improved_str = if pruning { " (improved pruning)" } else { "" };
+        dao_bench_solver(
+            c,
+            format!("JPS{improved_str}").as_str(),
+            |pathing_grid: &mut PathingGrid<ALLOW_DIAGONAL>| {
+                let mut solver = JPSSolver::new(&pathing_grid, pruning);
+                solver.initialize(&pathing_grid);
+                solver
+            },
+        );
     }
+}
+
+fn dao_bench_alt<const ALLOW_DIAGONAL: bool>(c: &mut Criterion) {
+    dao_bench_solver(
+        c,
+        "ALT (MC)",
+        |pathing_grid: &mut PathingGrid<ALLOW_DIAGONAL>| {
+            let landmarks = generate_landmarks_mc(&pathing_grid, 64);
+            ALTSolver::new(landmarks, pathing_grid)
+        },
+    );
 }
 
 fn dao_bench_astar<const ALLOW_DIAGONAL: bool>(c: &mut Criterion) {
-    let bench_set = ["dao/arena", "dao/den009d", "dao/den312d"];
-    for name in bench_set {
-        let (bool_grid, scenarios) = get_benchmark(name.to_owned());
-        let mut pathing_grid: PathingGrid<ALLOW_DIAGONAL> =
-            PathingGrid::new(bool_grid.width, bool_grid.height, true);
-        pathing_grid.grid = bool_grid.clone();
-        pathing_grid.generate_components();
-        let solver = AstarSolver::new();
-        let diag_str = if ALLOW_DIAGONAL { "8-grid" } else { "4-grid" };
-
-        c.bench_function(format!("{name}, Astar {diag_str}").as_str(), |b| {
-            b.iter(|| {
-                for (start, end, _) in &scenarios {
-                    black_box(solver.get_path_single_goal(&mut pathing_grid, *start, *end));
-                }
-            })
-        });
-    }
+    dao_bench_solver(c, "Astar", |_: &mut PathingGrid<ALLOW_DIAGONAL>| {
+        AstarSolver::new()
+    });
 }
-fn dao_bench_dijkstra<const ALLOW_DIAGONAL: bool>(c: &mut Criterion) {
-    let bench_set = ["dao/arena", "dao/den009d", "dao/den312d"];
-    for name in bench_set {
-        let (bool_grid, scenarios) = get_benchmark(name.to_owned());
-        let mut pathing_grid: PathingGrid<ALLOW_DIAGONAL> =
-            PathingGrid::new(bool_grid.width, bool_grid.height, true);
-        pathing_grid.grid = bool_grid.clone();
-        pathing_grid.generate_components();
-        let solver = DijkstraSolver;
-        let diag_str = if ALLOW_DIAGONAL { "8-grid" } else { "4-grid" };
 
-        c.bench_function(format!("{name}, Dijkstra {diag_str}").as_str(), |b| {
-            b.iter(|| {
-                for (start, end, _) in &scenarios {
-                    black_box(solver.get_path_single_goal(&mut pathing_grid, *start, *end));
-                }
-            })
-        });
-    }
+fn dao_bench_dijkstra<const ALLOW_DIAGONAL: bool>(c: &mut Criterion) {
+    dao_bench_solver(c, "Dijkstra", |_: &mut PathingGrid<ALLOW_DIAGONAL>| {
+        DijkstraSolver
+    });
 }
 
 criterion_group!(
     benches,
     // dao_bench<false>,
-    dao_bench<true>,
+    // dao_bench<true>,
     // dao_bench_jps<false>,
-    // dao_bench_jps<true>,
-    dao_bench_alt<true>,
-    // dao_bench_astar<false>,
+    dao_bench_jps<false>,
+    dao_bench_alt<false>,
+    dao_bench_astar<false>,
     // dao_bench_astar<true>,
     // dao_bench_dijkstra<false>,
     // dao_bench_dijkstra<true>
